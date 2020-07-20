0761d4da523c76b16aaf21035a01cf6b06dfa7ac
Jonathan Dalrymple <jdalrymple@marqeta.com>
2020-07-20 10:44:09 +0100

    Updates

---
+++ precache-manifest.fd65adc555ad9ad8412e0c962703359b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c55994a88563f6d39e146b71be518b",
+    "url": "/isthisagoodinvestment/index.html"
+  },
+  {
+    "revision": "ffbdcfeb468a4562a487",
+    "url": "/isthisagoodinvestment/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "df97c089d188d4695b34",
+    "url": "/isthisagoodinvestment/static/js/2.576c2944.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/isthisagoodinvestment/static/js/2.576c2944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbdcfeb468a4562a487",
+    "url": "/isthisagoodinvestment/static/js/main.6e27766b.chunk.js"
+  },
+  {
+    "revision": "1a6a843d01fb4bed1a22",
+    "url": "/isthisagoodinvestment/static/js/runtime-main.5c365a98.js"
+  }
+]);
